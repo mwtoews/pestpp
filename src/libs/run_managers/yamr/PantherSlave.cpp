803e0652ee34fc44e0dc44f8e51b26f75e14b5ab
Jeremy White <j.white@gns.cri.nz>
2018-09-13 12:20:39 +1200

    fixed issue with yamr poll interval on slave side

--- src/libs/run_managers/yamr/PantherSlave.cpp
+++ src/libs/run_managers/yamr/PantherSlave.cpp
@@ -284,7 +284,38 @@ void PANTHERSlave::process_panther_ctl_file(const string &ctl_filename)
 		e.add_front(out.str());
 		e.raise();
 	}
+
 	fin.close();
+	poll_interval_seconds = 1;
+	for (auto &line : pestpp_lines)
+	{
+		string key;
+		string value;
+		regex lambda_reg("(\\w+)(?:\\s*\\()([^\\)]+)(?:\\))");
+		const std::sregex_iterator end_reg;
+		//regex lambda_reg("((\\w+\\s*\\([^\\)]+\\))+?)");
+		cmatch mr;
+
+		size_t found = line.find_first_of("#");
+		if (found == string::npos) {
+			found = line.length();
+		}
+		string tmp_line = line.substr(0, found);
+		strip_ip(tmp_line, "both", "\t\n\r+ ");
+		//upper_ip(tmp_line);
+
+		for (std::sregex_iterator i(tmp_line.begin(), tmp_line.end(), lambda_reg); i != end_reg; ++i)
+		{
+			string key = (*i)[1];
+			string org_value = (*i)[2];
+			upper_ip(key);
+			string value = upper_cp(org_value);
+			if (key == "YAMR_POLL_INTERVAL") {
+				convert_ip(value, poll_interval_seconds);
+
+			}
+		}
+	}
 
 
 
